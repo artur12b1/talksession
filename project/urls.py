58dbe50f599c822640eba35d2a516980edda7538
artur12b1 <artur12b1@gmail.com>
2026-05-29 10:08:20 +0200

    static change

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('talksession.urls')),
@@ -27,5 +26,4 @@ urlpatterns = [
 
 handler404 = 'talksession.views.custom_404_view'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
